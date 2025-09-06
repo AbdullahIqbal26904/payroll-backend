6f4af21a3b66d0758ba71c17f1c1fde479c96eb2
Abdullah Iqbal <abdullah.iqbal1505@gmail.com>
2025-09-06 17:23:39 +0500

    holidays routes corrected.

--- routes/holidayRoutes.js
+++ routes/holidayRoutes.js
@@ -5,12 +5,16 @@ const { protect, authorize } = require('../middlewares/auth');
 
 // Routes for holiday management
 router.post('/', protect, authorize('admin'), holidayController.addHoliday);
-router.put('/:id', protect, authorize('admin'), holidayController.updateHoliday);
-router.delete('/:id', protect, authorize('admin'), holidayController.deleteHoliday);
 router.get('/', protect, holidayController.getAllHolidays);
-router.get('/range', protect, holidayController.getHolidaysInRange);
+
+// Settings routes - specific routes before param routes
 router.get('/settings', protect, authorize('admin'), holidayController.getHolidaySettings);
 router.put('/settings', protect, authorize('admin'), holidayController.updateHolidaySettings);
+router.get('/range', protect, holidayController.getHolidaysInRange);
+
+// Routes with parameters should come after specific routes
+router.put('/:id', protect, authorize('admin'), holidayController.updateHoliday);
+router.delete('/:id', protect, authorize('admin'), holidayController.deleteHoliday);
 router.get('/:id', protect, holidayController.getHolidayById);
 
 module.exports = router;
