0303c35053da265b806ddd2cde570873a193aa2b
Abdullah Iqbal <abdullah.iqbal1505@gmail.com>
2025-10-01 01:07:59 +0500

    deploy

--- app.js
+++ app.js
@@ -27,7 +27,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: ['https://payroll-system-liard.vercel.app','https://msa.medsaas.me/', 'http://localhost:3000', 'https://nodejs.msa.medsaas.me'],
+  origin: ['https://payroll-system-liard.vercel.app','https://msa.medsaas.me', 'http://localhost:3000', 'https://nodejs.msa.medsaas.me'],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
